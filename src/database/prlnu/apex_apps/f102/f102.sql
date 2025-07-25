eec2ae25c1259b5b91b3610b3f871156f0d8d034
Prabhakar <prabhakar.lnu@oracle.com>
2025-07-25 13:25:50 +0530

    milestone-2 change in dashboard page heading test

--- src/database/prlnu/apex_apps/f102/f102.sql
+++ src/database/prlnu/apex_apps/f102/f102.sql
@@ -155,7 +155,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_value_04=>'Customers'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>31
-,p_version_scn=>187959776
+,p_version_scn=>39431407302142
 ,p_print_server_type=>'INSTANCE'
 ,p_file_storage=>'DB'
 ,p_is_pwa=>'N'
@@ -10367,7 +10367,7 @@ wwv_flow_imp_page.create_page(
 );
 wwv_flow_imp_page.create_page_plug(
  p_id=>wwv_flow_imp.id(1079360011101406985)
-,p_plug_name=>'Customers by Category'
+,p_plug_name=>'Customers by Category-helllooo'
 ,p_region_name=>'CUST_BY_CATS'
 ,p_region_css_classes=>'scrollable-region'
 ,p_region_template_options=>'#DEFAULT#:js-showMaximizeButton:i-h320:t-Region--scrollBody'
@@ -74064,4 +74064,4 @@ prompt  ...done
 
 
 
--- sqlcl_snapshot {"hash":"0a66b679cb51b47191dba7497dc65ea2531185a9","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
+-- sqlcl_snapshot {"hash":"7b6f074fd1a4e3a4891c65fd15de5c6809531c5d","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
